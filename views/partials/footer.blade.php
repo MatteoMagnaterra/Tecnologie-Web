fdc4d1ec98521cfca4303819450c24abb1442a12
MatteoMagnaterra <S1109659@studenti.univpm.it>
2025-05-13 11:28:01 +0200

    Add files via upload

---
+++ views/partials/footer.blade.php
@@ -0,0 +1,29 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-light mt-5 py-5 footer-custom">
+    <div class="container py-1">
+        <div class="row g-5">
+            <div class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center">
+                <a href="homepage.html" class="navbar-brand">
+                    <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>Health Center</h1>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Mettiti in contatto</h4>
+                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>I Settembre, Ancona, Italia</p>
+                <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>health.center@gmail.com</p>
+                <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Popular Links</h4>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Homepage</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Chi siamo</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Dipartimenti</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Specialisti</a>
+                    <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Log in</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
