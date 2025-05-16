0fb47c0cb02fc7915a526cae7f12fab946bf2652
MatteoMagnaterra <S1109659@studenti.univpm.it>
2025-05-16 11:19:18 +0200

    web.php

--- web.php
+++ web.php
@@ -2,14 +2,13 @@
 
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\SpecialistiController;
-use App\Http\Controllers\EsempioController;
 
 Route::get('/', [HomepageController::class, 'index'])
         ->name('homepage');
 
 
 Route::view('/chisiamo', 'chisiamo')->name('chisiamo');
-Route::get('/specialisti', [EsempioController::class, 'index'])->name('specialisti');
+Route::get('/specialisti', [SpecialistiController::class, 'index'])->name('specialisti');
 Route::view('/dipartimenti', 'dipartimenti')->name('dipartimenti');
 Route::view('/cardiologia', 'cardiologia')->name('cardiologia');
 Route::view('/neurologia', 'neurologia')->name('neurologia');
