0478ecfd824b16e667cf2943e8186f1358ea210f
MatteoMagnaterra <S1109659@studenti.univpm.it>
2025-05-21 10:55:52 +0200

    Update web.php

--- Progetto/web.php
+++ Progetto/web.php
@@ -2,15 +2,21 @@
 
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\DipartimentiController;
+use App\Http\Controllers\SpecialistiController;
 
 Route::get('/', [HomepageController::class, 'index'])
         ->name('homepage');
 
 
 Route::view('/chisiamo', 'chisiamo')->name('chisiamo');
-Route::view('/specialisti', 'specialisti')->name('specialisti');
+Route::get('/specialisti', [SpecialistiController::class, 'mostraSpec'])
+        ->name('specialisti');
 Route::get('/dipartimenti', [DipartimentiController::class, 'mostraDip'])
         ->name('dipartimenti');
+Route::get('/dipartimenti/{id}', [DipartimentiController::class, 'show'])
+        ->name('dipartimenti1');
+Route::get('/prestazione/{id}', [DipartimentiController::class, 'mostraPre'])
+        ->name('prestazioni');
 Route::view('/cardiologia', 'cardiologia')->name('cardiologia');
 Route::view('/neurologia', 'neurologia')->name('neurologia');
 Route::view('/ortopedia', 'ortopedia')->name('ortopedia');
