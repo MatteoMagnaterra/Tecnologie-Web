9be7150f8cc418ec5bebd3b64d3fdf1d1b950389
MatteoMagnaterra <S1109659@studenti.univpm.it>
2025-05-21 10:57:26 +0200

    Update DipartimentiController.php

--- Progetto/Http/Controllers/DipartimentiController.php
+++ Progetto/Http/Controllers/DipartimentiController.php
@@ -4,20 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Dipartimento;
+use App\Models\Prestazione;
 
 class DipartimentiController extends Controller
 {
     public function mostraDip(Request $request)
     {
-        $query = Dipartimento::select('nome', 'descrizione');
-        
+        $query = Dipartimento::select('nome', 'descrizione', 'idDip');
+
         if ($request->has('search')) {
-            $searchteam = $request->input('search');
-            $query->where('nome', 'LIKE', '%' . $searchteam . '%');
+            $search = $request->input('search');
+
+            if (str_ends_with($search, '*')) {
+                $prefix = rtrim($search, '*');
+                $query->where('nome', 'LIKE', $prefix . '%');
+            } else {
+                $query->where('nome', 'LIKE', '%' . $search . '%');
+            }
         }
-        
+
         $dipartimenti = $query->get();
-        
-        return view ('dipartimenti', compact('dipartimenti'));
+
+        return view('dipartimenti', compact('dipartimenti'));
     }
+
+    
+    public function show($id)
+    {
+        $dipartimento = Dipartimento::with('prestazioni', 'specialisti')->findOrFail($id);
+    
+        return view('dipartimenti1', compact('dipartimento'));
+    }
+    
+    public function mostraPre($id)
+    {
+        $prestazione = Prestazione::with(['dipartimento', 'specialista'])->findOrFail($id);
+        return view('prestazioni', compact('prestazione'));
+    }
+
 }
